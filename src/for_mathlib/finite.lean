f6ff34c93fc534f4fa98615670601198c4877a41
Reid Barton <rwbarton@gmail.com>
2020-08-06 14:48:41 -0400

    chore: add type annotations

--- src/for_mathlib/finite.lean
+++ src/for_mathlib/finite.lean
@@ -1,5 +1,4 @@
 import data.set.finite
-import data.fintype.basic
 
 variables {α : Type*} {β : Type*}
 
@@ -14,12 +13,13 @@ sorry
 lemma Ioo.infinite [preorder α] [densely_ordered α] {x y : α} (h : x < y) :
   infinite (Ioo x y) :=
 begin
-  obtain ⟨c, hc₁, hc₂⟩ := dense h,
+  obtain ⟨c, hc₁, hc₂⟩ : ∃ c : α, x < c ∧ c < y := dense h,
   intro f,
   letI := f.fintype,
-  have := fintype.well_founded_of_trans_of_irrefl ((<) : Ioo x y → Ioo x y → Prop),
-  obtain ⟨m, _, hm⟩ := this.has_min univ ⟨⟨c, hc₁, hc₂⟩, trivial⟩,
-  obtain ⟨z, hz₁, hz₂⟩ := dense m.2.1,
+  have : well_founded ((<) : Ioo x y → Ioo x y → Prop) :=
+    fintype.well_founded_of_trans_of_irrefl _,
+  obtain ⟨m, _, hm⟩ : ∃ (m : Ioo x y) _, ∀ d ∈ univ, ¬ d < m := this.has_min univ ⟨⟨c, hc₁, hc₂⟩, trivial⟩,
+  obtain ⟨z, hz₁, hz₂⟩ : ∃ (z : α), x < z ∧ z < m := dense m.2.1,
   refine hm ⟨z, hz₁, lt_trans hz₂ m.2.2⟩ trivial hz₂
 end
 
