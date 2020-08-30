147b1d8aea27e1d78fb9c83b25c7fad336d9c5c9
Reid Barton <rwbarton@gmail.com>
2020-08-30 16:39:44 -0400

    chore: bump lean, mathlib

--- src/for_mathlib/fin.lean
+++ src/for_mathlib/fin.lean
@@ -1,17 +1,17 @@
 import data.equiv.fin
 
 lemma fin.cast_id {n : ℕ} {h : n = n} : fin.cast h = id :=
-by { ext, apply fin.cast_val }
+by { ext, refl }
 
 @[simp] lemma fin.nat_add_val {n m : ℕ} (i : fin n) : (i.nat_add m).val = m + i.val :=
 rfl
 
 lemma fin.nat_add_zero {n : ℕ} : fin.nat_add 0 = fin.cast (zero_add n).symm :=
-by { ext, rw fin.cast_val, apply zero_add }
+by { ext, apply zero_add }
 
 lemma fin.nat_add_add {n m k : ℕ} :
   fin.nat_add (m+k) = fin.cast (add_assoc m k n).symm ∘ fin.nat_add m ∘ fin.nat_add k :=
-by { ext, rw fin.cast_val, apply add_assoc }
+by { ext, apply add_assoc }
 
 @[simp] lemma sum_fin_sum_equiv_inl {m n : ℕ} (i : fin m) :
   sum_fin_sum_equiv (sum.inl i) = fin.cast_add n i :=
