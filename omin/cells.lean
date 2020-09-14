ef035de1570dd73980ab3a6ac63807ce56e557ee
Reid Barton <rwbarton@gmail.com>
2020-09-14 19:41:49 -0400

    sketch(cells): port to o_minimal

--- omin/cells.lean
+++ omin/cells.lean
@@ -1,13 +1,13 @@
-import .ominimal
+import o_minimal.o_minimal
 import data.set.disjointed
 import data.setoid.partition
 
-section omin
+namespace o_minimal
 
 local notation R `^` n := fin n → R
 
-variables {R : Type*} [nonempty R] [decidable_linear_order R] [densely_ordered R] [no_bot_order R] [no_top_order R]
-variables (S : struc R) [definable_constants S] [definable_lt S]
+variables {R : Type*} [DUNLO R]
+variables (S : struc R) [definable_constants S] [is_definable_le S R]
 
 def init {n} : R^(n+1) → R^n := λ x, x ∘ fin.cast_succ
 
@@ -18,28 +18,28 @@ def cell : Π {n}, set (R^n) → Prop
 | (n+1) s := ∃ (s₀ : set (R^n)) (h : cell s₀),
                   s = {x | (init x) ∈ s₀}
               ∨
-              (∃ (f : R^n → R) (hf : S.definable_fun f),
+              (∃ (f : R^n → R) (hf : def_fun S f),
                   s = {x | (init x) ∈ s₀ ∧ x (fin.last n) = f (init x)}
                 ∨ s = {x | (init x) ∈ s₀ ∧ x (fin.last n) ∈ set.Ioi (f (init x))}
                 ∨ s = {x | (init x) ∈ s₀ ∧ x (fin.last n) ∈ set.Iio (f (init x))})
               ∨
-              (∃ (f g : R^n → R) (hf : S.definable_fun f) (hg : S.definable_fun g),
+              (∃ (f g : R^n → R) (hf : def_fun S f) (hg : def_fun S g),
                 (∀ x₀ ∈ s₀, f x₀ < g x₀) ∧
                   s = {x | (init x) ∈ s₀ ∧ x (fin.last n) ∈ set.Ioo (f (init x)) (g (init x))})
 
 lemma init_definable (n : ℕ) :
-  S.definable_fun (init : R^(n+1) → R^n) :=
+  def_fun S (init : R^(n+1) → R^n) :=
 begin
   sorry
 end
 
-lemma cell.definable : ∀ {n} (s : set (R^n)) (hs : S.cell s), S.definable s
-| 0     s hs := by { cases hs, exact S.definable_univ }
+lemma cell.definable : ∀ {n} (s : set (R^n)) (hs : S.cell s), def_set S s
+| 0     s hs := by { cases hs, apply def_set_univ }
 | (n+1) s hs :=
 begin
   obtain ⟨s₀, hs₀, H|H|H⟩ := hs,
   { cases H,
-    show S.definable (init ⁻¹' s₀), sorry },
+    show def_set S (init ⁻¹' s₀), sorry },
   sorry,
   sorry,
 end
@@ -52,4 +52,4 @@ def decomposition : Π {n} (C : set (set (R^n))), Prop
 
 end struc
 
-end omin
+end o_minimal
