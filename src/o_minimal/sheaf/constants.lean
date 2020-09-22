348b5e77b37ec54b5935cee050d1ca93a0999738
Reid Barton <rwbarton@gmail.com>
2020-09-22 17:04:48 -0400

    feat(o_minimal/sheaf/constants): some basic definability lemmas

---
+++ src/o_minimal/sheaf/constants.lean
@@ -0,0 +1,172 @@
+import o_minimal.sheaf.tactic
+
+namespace o_minimal
+
+variables {R : Type*} {S : struc R}
+variables {X Y Z : Type*} [definable_sheaf S X] [definable_sheaf S Y] [definable_sheaf S Z]
+
+lemma definable.const : definable S (@function.const X Y) :=
+begin [defin]
+  intro x,
+  intro y,
+  var
+end
+
+lemma definable.comp : definable S (@function.comp X Y Z) :=
+begin [defin]
+  intro f,
+  intro g,
+  intro x,
+  app, var,
+  app, var, var
+end
+
+lemma definable.prod_mk : definable S (@prod.mk X Y) :=
+begin [defin]
+  intro x,
+  intro y,
+  exact ⟨x.definable, y.definable⟩
+end
+
+lemma definable.fst : definable S (prod.fst : X × Y → X) :=
+begin [defin]
+  intro p,
+  exact p.definable.1
+end
+
+lemma definable.snd : definable S (prod.snd : X × Y → Y) :=
+begin [defin]
+  intro p,
+  exact p.definable.2
+end
+
+instance punit.definable_sheaf : definable_sheaf S punit :=
+{ definable := λ K f, true,
+  definable_precomp := λ L K φ f hf, trivial }
+
+lemma definable.star : definable S punit.star :=
+begin [defin]
+  exact trivial
+end
+
+instance Prop.definable_sheaf : definable_sheaf S Prop :=
+{ definable := λ K f, def_set S f,
+  definable_precomp := λ L K φ f hf, φ.is_definable.preimage hf }
+
+lemma definable.and : definable S and :=
+begin [defin]
+  intro p,
+  intro q,
+  exact def_set.inter p.definable q.definable
+end
+
+lemma definable.or : definable S or :=
+begin [defin]
+  intro p,
+  intro q,
+  exact def_set.union p.definable q.definable
+end
+
+lemma definable.imp : definable S ((→) : Prop → Prop → Prop) :=
+begin [defin]
+  intro p,
+  intro q,
+  exact def_set.imp p.definable q.definable
+end
+
+lemma definable.not : definable S not :=
+begin [defin]
+  intro p,
+  exact def_set.compl p.definable
+end
+
+instance set.definable_sheaf : definable_sheaf S (set X) :=
+show definable_sheaf S (X → Prop), by apply_instance
+
+lemma definable.mem : definable S ((∈) : X → set X → Prop) :=
+begin [defin]
+  intro x,
+  intro s,
+  app, var, var
+end
+
+lemma definable.inter : definable S ((∩) : set X → set X → set X) :=
+begin [defin]
+  intro s,
+  intro t,
+  intro x,
+  app,
+  app,
+  exact definable.and.definable _,
+  app, app, exact definable.mem.definable _, var, var,
+  app, app, exact definable.mem.definable _, var, var
+end
+
+lemma definable.union : definable S ((∪) : set X → set X → set X) :=
+begin [defin]
+  intro s,
+  intro t,
+  intro x,
+  app,
+  app,
+  exact definable.or.definable _,
+  app, app, exact definable.mem.definable _, var, var,
+  app, app, exact definable.mem.definable _, var, var
+end
+
+lemma definable.diff : definable S ((\) : set X → set X → set X) :=
+begin [defin]
+  intro s,
+  intro t,
+  intro x,
+  app, app, exact definable.and.definable _,
+  app, app, exact definable.mem.definable _, var, var,
+  -- unnecessarily complicated, but hey why not
+  -- TODO: `change`
+  app,
+  intro a,
+  app, exact definable.not.definable _,
+  app, app, exact definable.mem.definable _, var, var,
+  var
+end
+
+-- Quantification over X is not definable in general.
+-- Needs a special property of X: "quasicompactness"?
+
+/-
+lemma definable.subset : definable S ((⊆) : set X → set X → Prop) :=
+begin [defin]
+  intro s,
+  intro t,
+end
+
+lemma definable.powerset : definable S (set.powerset : set X → set (set X)) :=
+begin [defin]
+  intro s,
+  intro t,
+end
+-/
+
+instance subtype.definable_sheaf {s : set X} : definable_sheaf S s :=
+{ definable := λ K f, definable_sheaf.definable (subtype.val ∘ f),
+  definable_precomp := λ L K φ f hf,
+    definable_sheaf.definable_precomp φ (subtype.val ∘ f) hf }
+
+-- instance prop.definable_sheaf {p : Prop} : definable_sheaf S p := sorry
+
+-- TODO: With an instance for Pi types, can we state the definable dependence on `s`?
+lemma definable.subtype.val {s : set X} : definable S (subtype.val : s → X) :=
+begin [defin]
+  intro v,
+  exact v.definable
+end
+
+-- How to state definable subtype.mk?
+/-
+lemma definable.subtype.mk {s : set X} :
+  definable S (subtype.mk : Π (x : X), x ∈ s → s) :=
+begin
+end
+-/
+
+end o_minimal
